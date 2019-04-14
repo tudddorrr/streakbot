ca8a03c8c33172fe73fbce6ef671fb7bfe4e3b26
Tudor <sekaru@sleepystudios.net>
2019-04-14 12:04:32 +0100

    Channel management

---
+++ services/channels.js
@@ -0,0 +1,42 @@
+db = require('../db')
+
+canManageChannels = member => {
+  // the bitfield of the permissions are all the permissions the user has
+  // and in this case i'm checking if the flag corresponding to having 
+  // administrator permissions is set.
+  // see https://discordapp.com/developers/docs/topics/permissions
+  return member.permissions.bitfield & 0x00000008
+}
+
+const CHANNEL_MANAGEMENT_NOT_ENOUGH_PERMISSIONS = 'you must be an admin to use this command.' 
+const CHANNEL_MANAGEMENT_FORMATTING = `please use the command like this: \`!setchannels channel1, channel2, channel3, etc\`.`
+const CHANNEL_MANAGEMENT_SUCCESS = `your streak channels have been successfully updated!`
+const CHANNEL_MANAGEMENT_ERROR = `one or more channels you specified do not exist. Your streak channels were not updated.`
+
+exports.handleChannels = msg => {
+  if (!canManageChannels(msg.member)) {
+    msg.reply(CHANNEL_MANAGEMENT_NOT_ENOUGH_PERMISSIONS)
+    return
+  } 
+
+  const channels = msg.content.substring(12)
+  if(channels.length === 0) {
+    msg.reply(CHANNEL_MANAGEMENT_FORMATTING)
+    return
+  }
+
+  let channelsArray = channels.split(',')
+  channelsArray = channelsArray.map(channel => {
+    return channel.trim()
+  })
+
+  for(let channel of channelsArray) {
+    if(!msg.guild.channels.find(gc => gc.name === channel)) {
+      msg.reply(CHANNEL_MANAGEMENT_ERROR)
+      return
+    }
+  }
+
+  db.addChannels(channelsArray, msg.guild.id)
+  msg.reply(CHANNEL_MANAGEMENT_SUCCESS)
+}
